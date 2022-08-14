4ad39d6b10ca6ef4d4ca420e62d5873286daa628
ctaylor0002 <charles4312@comcast.net>
2022-08-13 22:38:40 -0400

    Created grab_random_weapon function for the Robot class.

--- robot.py
+++ robot.py
@@ -1,5 +1,5 @@
 # (10 points): As a developer, I want a Robot to have a name, health, and active_weapon.
-
+import random
 class Robot:
 
     def __init__(self, name, total_health, weapon):
@@ -8,3 +8,6 @@ class Robot:
         self.active_weapon = weapon
 
     # Create a function here to grab a random weapon from a list and set it to this robots weapon.
+    def grab_random_weapon(self, weapon_list):
+        random_index = random.randrange(0,len(weapon_list))
+        self.active_weapon = weapon_list[random_index]
