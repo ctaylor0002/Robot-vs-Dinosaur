94231244f59ba8d51fe540adda67cffeb0e3bf1a
ctaylor0002 <charles4312@comcast.net>
2022-08-13 22:13:16 -0400

    Created the class Weapon and gave it's __init__ function.

---
+++ weapon.py
@@ -0,0 +1,9 @@
+# (10 points): As a developer, I want a Weapon to have a name and attack_power.
+
+class Weapon:
+
+    def __init__(self, weapon_name, attack_power):
+        self.weapon_name = weapon_name
+        self.attack_power = attack_power
+
+    
